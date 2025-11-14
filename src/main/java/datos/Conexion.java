5fb3685a2853ec717f567014ee3692188f6c2c21
mendozamonic <monicafrancomendoza@gmail.com>
2025-11-13 19:34:48 -0600

    Conexion con la base de datos

--- src/main/java/datos/Conexion.java
+++ src/main/java/datos/Conexion.java
@@ -8,6 +8,22 @@ package datos;
  *
  * @author monic
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException; 
+
 public class Conexion {
+       // CONFIGURACIONN  ------ MODIFIQUEN ESTO SEGUN LA INFORMACIÓN SEGUN SU PUERTO-CONTRSEÑA 
+    private static final String URL = "jdbc:postgresql://localhost:5434/proyecto";
+    private static final String USER = "postgres";
+    private static final String PASS = "hdp";  
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("No se encontró el Driver de PostgreSQL", e);
+        }
+    }
     
-}
+    }
